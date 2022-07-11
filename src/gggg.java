b09c372124fe3b1ab444e0b0dddff4bb24c5a347
as <aayaroglu@gmail.com>
2022-07-11 11:07:59 +0300

    ikinci

--- src/gggg.java
+++ src/gggg.java
@@ -1,5 +1,8 @@
 public class gggg {
     public static void main(String[] args) {
-        System.out.println("dsgsgsgsgsgs");
+
+
+        System.out.println("gehejahjsj merhaba 2");
+        System.out.println("nfffffffff 3");
     }
 }
